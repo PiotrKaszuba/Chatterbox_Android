cb50bcabcf80a669491a4b6924f45d5ec8319151
ffedec <m.fedec11@gmail.com>
2018-03-27 15:19:17 +0200

    userListDziala/Logout zrobiony tez

--- app/src/main/java/com/put/Chatterbox/Controller/UserList.java
+++ app/src/main/java/com/put/Chatterbox/Controller/UserList.java
@@ -3,9 +3,13 @@ package com.put.Chatterbox.Controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.put.Chatterbox.Model.User;
 import com.put.Chatterbox.R;
 
@@ -13,6 +17,7 @@ import java.util.ArrayList;
 
 public class UserList extends AppCompatActivity {
 
+    TextView logoutView;
     User user;
     ListView userListView;
     ArrayAdapter<String> adapter;
@@ -28,9 +33,15 @@ public class UserList extends AppCompatActivity {
 
     public void updateUserList(ArrayList<String> userList) {
         userListView = (ListView) findViewById(R.id.userListView);
+        logoutView = (TextView) findViewById(R.id.logoutButton);
         adapter = new ArrayAdapter<String>(this,R.layout.list_view_row,userList);
 
         userListView.setAdapter(adapter);
     }
 
+    protected void SignOut(View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+    }
 }
