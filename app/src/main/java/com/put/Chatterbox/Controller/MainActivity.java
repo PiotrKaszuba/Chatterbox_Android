879456cff1f03add76da13d24e74d63860281b64
ffedec <m.fedec11@gmail.com>
2018-03-29 13:03:52 +0200

    forgot password

--- app/src/main/java/com/put/Chatterbox/Controller/MainActivity.java
+++ app/src/main/java/com/put/Chatterbox/Controller/MainActivity.java
@@ -2,13 +2,19 @@ package com.put.Chatterbox.Controller;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.put.Chatterbox.R;
 
@@ -44,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
     protected void forgotPassword(View view){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
-        alert.setTitle("Title");
-        alert.setMessage("Message");
+        alert.setTitle("We will send you link to reset password");
+        alert.setMessage("Your email:");
 
 // Set an EditText view to get user input
         final EditText input = new EditText(this);
@@ -53,8 +59,21 @@ public class MainActivity extends AppCompatActivity {
 
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
-
-                // Do something with value!
+            String s = input.getText().toString();
+            int a =1;
+                FirebaseAuth.getInstance().sendPasswordResetEmail(s)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Email sent",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    Toast.makeText(getApplicationContext(), "Wrong email",
+                                            Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
